50d0873de0b753d6b8dd2fd4cd6c78921411d706
Joaquin Degiovanni <122236268+joackoodeg@users.noreply.github.com>
2024-05-14 20:39:00 -0300

    Update 10.rkt

--- 10.rkt
+++ 10.rkt
@@ -11,28 +11,3 @@
 
 (cantidad 3 '(1 2 3 4 5 4 3 2 1 ))
 
-;profe:
-
-(define contar
-  (lambda (e l)
-    (if (null? l)
-      0
-      (if(eq? (car l) e)
-      (+ (contar e (cdr l)) 1 )
-      (contar e (cdr l))
-      )
-    )
-  )
-)
-
-(define eliminar
-  (lambda (x ls)
-    (if(null? ls)
-       ls
-    (if (eq? x (car ls))
-        (eliminar x (cdr ls))
-        (cons (car ls)(eliminar x (cdr ls)))
-   )    
-  )
- )
-)
